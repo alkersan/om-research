32dd5a5e5c8f82eeaebb3154a6e5192d325d4327
Dmytro Aleksandrov <alkersan@gmail.com>
2026-02-02 19:04:56 -0500

    tested on OM4

--- rotate.py
+++ rotate.py
@@ -2,29 +2,27 @@ import asyncio
 
 from gimbal import Gimbal
 
-OM2_ADDRESS = "78:04:73:C6:41:5F"  # OM2
-OM3_ADDRESS = "FC:6A:72:D9:FC:E4"  # OM3
+OM2_ADDRESS = "78:04:73:C6:41:5F"
+OM3_ADDRESS = "FC:6A:72:D9:FC:E4"
+OM4_ADDRESS = "FE:F9:F0:EE:65:DC"
 
 
 async def main():
     try:
-        om3 = Gimbal(OM3_ADDRESS)
-        await om3.connect()
-
-        yaw, pitch = -500, -500
-        for _ in range(30):
-            yaw, pitch = -yaw, -pitch
-            rotation = {
-                "mode": om3.RotationMode.RELATIVE,
-                "yaw": yaw, "pitch": pitch, "roll": 0,
-                "time": 7,
-            }
-            await om3.rotate(**rotation)
-            await asyncio.sleep(1)
+        gimbal = Gimbal(OM4_ADDRESS)
+        await gimbal.connect()
+        yaw = [-1500, 1500]
+        for i in range(30):
+            await gimbal.rotate(
+                mode=gimbal.RotationMode.SPEED,
+                yaw=yaw[i%len(yaw)], pitch=0, roll=0,
+                time=10
+            )
+            await asyncio.sleep(1.5)
     except asyncio.CancelledError:
         print("Task cancelled, cleaning up...")
     finally:
-        await om3.disconnect()
+        await gimbal.disconnect()
 
 
 if __name__ == '__main__':
